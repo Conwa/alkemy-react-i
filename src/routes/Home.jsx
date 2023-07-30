3c508dd6cdc14629dcb1b64a5909f5040a481d30
Conwa <conradodelcarlo97@gmail.com>
2023-07-29 21:19:35 -0300

    componente card casi listo

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,21 +3,26 @@ import Navbar from "../components/Navbar";
 
 import { useEffect, useState } from "react";
 
+import Card from "../components/Card";
 import moviesService from "../services/movies";
 
 export default function Home() {
   const [moviesList, setMoviesList] = useState();
   useEffect(() => {
-    moviesService.getList().then((movies) => setMoviesList([...movies]));
+    moviesService.getList().then((movies) => {
+      // const sliced = movies.slice(0, 2);
+      setMoviesList([...movies]);
+    });
   }, []);
 
   return (
-    <div className="h-screen max-h-screen flex flex-col" id="login-form">
+    <div className="min-h-screen flex flex-col" id="home-layout">
       <Navbar />
-      <div className="flex flex-row h-fit w-full">
+      <h1>sds</h1>
+      <div className="flex flex-row h-fit w-full px-52 py-3 gap-3 flex-wrap justify-evenly">
         {moviesList ? (
           moviesList.map((movie, index) => {
-            return <h1 key={index}>{movie.title}</h1>;
+            return <Card key={index} movie={movie} />;
           })
         ) : (
           <h1>sds</h1>
