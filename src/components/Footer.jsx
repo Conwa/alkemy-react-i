98dcbc1bf4732f4ec94ddc1d8cb6836ca691941b
Conwa <conradodelcarlo97@gmail.com>
2023-08-28 20:31:18 -0300

    responsive footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
+import { useMediaQuery } from "react-responsive";
+
 import githubIcon from "../assets/github.png";
 import LinkedInIcon from "../assets/linkedin.png";
 
 export default function Footer() {
+  const mediumLayout = useMediaQuery({
+    query: "(min-width: 768px)",
+  });
+
   return (
     <div
-      className="flex justify-between w-full items-center px-52 py-3"
+      className="flex justify-between md w-full items-center  px-4 md:px-10 lg:px-52 py-3"
       style={{
         backgroundColor: "#121829b5",
       }}
@@ -29,7 +35,13 @@ export default function Footer() {
           </div>
         </a>
       </div>
-      <p className="links-small">&copy; Lorenzo Conrado Del Carlo, 2023</p>
+      {mediumLayout ? (
+        <p className="links-small">&copy; Lorenzo Conrado Del Carlo, 2023</p>
+      ) : (
+        <p className="links-extra_small">
+          &copy; Lorenzo Conrado Del Carlo, 2023
+        </p>
+      )}
     </div>
   );
 }
